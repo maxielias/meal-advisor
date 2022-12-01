89e7b9dca22bba5e0bfdc1a8977cf807e9f54058
maxielias <maximilianocoltro@gmail.com>
2022-12-01 12:30:34 -0300

    First commit

--- meal_advisor/config_files/api_keys.py
+++
@@ -1,6 +0,0 @@
-google_user = "meal-advisor"
-google_api_key = "AIzaSyCwKuBTOohJitrQpQDtLnzwtN0G0x8iCZ4"
-
-nutritionix_app_id="be0d7e30"
-nutritionix_api_key="aa65acf1b4629d8395eb8202fb7f9718"
-
--- meal_advisor/config_files/config.js
+++ meal_advisor/config_files/config.js
@@ -1,11 +1,11 @@
 var config = {
     google_user : 'meal-advisor',
-    google_key : 'AIzaSyCwKuBTOohJitrQpQDtLnzwtN0G0x8iCZ4',
+    google_key : '',
     nutritionix_app_id : 'be0d7e30',
     nutritionix_api_key : 'aa65acf1b4629d8395eb8202fb7f9718',
-    db : 'meal_advisor',
-    user : 'postgres',
-    password : 'postgres',
-    localhost : 'localhost',
-    port : '5432'
+    db : '',
+    user : '',
+    password : '',
+    localhost : '',
+    port : ''
   }
